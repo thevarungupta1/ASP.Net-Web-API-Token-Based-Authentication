07160f9727b772d1a6f6b9e26f050485b9318740
Varun Gupta <thevarungupta5@gmail.com>
2018-11-14 23:47:48 +0530

    Initial Commit

---
+++ WebApplication103/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace WebApplication103.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ WebApplication103/Controllers/EmployeesController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace WebApplication103.Controllers
+{
+    [Authorize]
+    public class EmployeesController : ApiController
+    {
+        public IEnumerable<Employee> Get()
+        {
+            using (SampleDBContext context = new SampleDBContext())
+            {
+                return context.Employees.ToList();
+            }                
+        }
+    }
+}
